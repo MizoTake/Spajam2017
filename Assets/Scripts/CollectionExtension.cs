95f7c157b82782f8d77b734abdff0cdb06258a56
Hiroki-Tanigawa <hirokilink0914@gmail.com>
2017-06-17 16:33:35 +0900

    add SceneManager

---
+++ Assets/Scripts/CollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CollectionExtension
+{
+	public static bool IsAny<T> (this IEnumerable<T> collection) {
+		return (collection != null && collection.Any());
+	}
+
+	public static void ForEach<T> (this IEnumerable<T> collection, Action<T> action)
+	{
+		foreach (T t in collection) {
+			action (t);
+		}
+	}
+
+	public static void ForEach<T> (this IEnumerable<T> collection, Action<T, int> action)
+	{
+		int i = 0;
+		foreach (T t in collection) {
+			action (t, i++);
+		}
+	}
+}
