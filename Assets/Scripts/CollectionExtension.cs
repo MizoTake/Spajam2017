2d62ccac1bd1085e3ed187d20280d745ec0f1628
Hiroki-Tanigawa <hirokilink0914@gmail.com>
2017-06-17 16:35:42 +0900

    コレクション拡張メソッドのコメント追記

--- Assets/Scripts/CollectionExtension.cs
+++ Assets/Scripts/CollectionExtension.cs
@@ -4,12 +4,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// リストの拡張メソッド
+/// </summary>
 public static class CollectionExtension
 {
+	/// <summary>
+	/// コレクション内に要素があるか
+	/// </summary>
+	/// <returns><c>true</c> if is any the specified collection; otherwise, <c>false</c>.</returns>
+	/// <param name="collection">Collection.</param>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
 	public static bool IsAny<T> (this IEnumerable<T> collection) {
 		return (collection != null && collection.Any());
 	}
 
+	/// <summary>
+	/// 簡易foreach
+	/// </summary>
+	/// <param name="collection">Collection.</param>
+	/// <param name="action">Action.</param>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
 	public static void ForEach<T> (this IEnumerable<T> collection, Action<T> action)
 	{
 		foreach (T t in collection) {
@@ -17,6 +32,12 @@ public static class CollectionExtension
 		}
 	}
 
+	/// <summary>
+	/// 簡易foreach(index付き)
+	/// </summary>
+	/// <param name="collection">Collection.</param>
+	/// <param name="action">Action.</param>
+	/// <typeparam name="T">The 1st type parameter.</typeparam>
 	public static void ForEach<T> (this IEnumerable<T> collection, Action<T, int> action)
 	{
 		int i = 0;
