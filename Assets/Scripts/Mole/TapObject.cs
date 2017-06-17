7068d8e64860614e1c84acc7a7f07568aec6b0da
SHIDAKEN <hayashidax@gmail.com>
2017-06-17 22:59:05 +0900

    モグラたたきα版完成でふ 会場の他のチーム皆真剣にやっててワロタ

---
+++ Assets/Scripts/Mole/TapObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapObject : MonoBehaviour {
+
+    public bool isHeart;
+
+	// Use this for initialization
+	void Start () {
+        isHeart = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Taped()
+    {
+        isHeart = false;
+    }
+}
