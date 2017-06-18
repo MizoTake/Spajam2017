72d07ba31a287ef5969539a850daf97538f7e0ef
Hiroki-Tanigawa <hirokilink0914@gmail.com>
2017-06-18 10:38:54 +0900

    SceneChangeButton 追加

---
+++ Assets/Scripts/SceneChangeButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// シーン遷移するボタン
+/// </summary>
+[RequireComponent (typeof(Button))]
+public class SceneChangeButton : MonoBehaviour
+{
+	[SerializeField]
+	private string _gotoSceneName;
+
+	private Button _button;
+
+	private void Start ()
+	{
+		_button = GetComponent<Button> ();
+		if (_button != null) {
+			_button.onClick.AddListener (() => {
+				SceneManager.LoadScene (_gotoSceneName);
+			});
+		}
+	}
+}
--- Assets/Scripts/Title/TitleManager.cs
+++ Assets/Scripts/Title/TitleManager.cs
@@ -5,13 +5,4 @@ using UnityEngine.UI;
 
 public class TitleManager : Singleton<TitleManager>
 { 
-	[SerializeField]
-	private Button _button;
-
-	private void Start ()
-	{
-		_button.onClick.AddListener (() => {
-			SceneManager.LoadScene ("Select");
-		});
-	}
 }
